95e8f1faf1ce19dec9459d293a15f6fa4f40b3c0
thibaut <dovonouthibaut3@gmail.com>
2023-07-21 17:59:04 +0100

    bug

--- src/router/index.js
+++ src/router/index.js
@@ -16,7 +16,7 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: "/",
+      path: "/acceuil",
       name: "home",
       component: accueil,
     },
