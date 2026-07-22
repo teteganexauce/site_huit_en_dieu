cf95ebdeb46334b5a5b65284de7cd8514262b53d
GBEDE Bernard <gbedebernard60@gmail.com>
2026-07-22 16:40:27 +0100

    feat: icônes LinkedIn et WhatsApp dans le footer

--- src/includes/footer.vue
+++ src/includes/footer.vue
@@ -14,10 +14,12 @@
                 <span v-if="siteStore.settings.HORAIRE"><strong>Horaires :</strong> {{ siteStore.settings.HORAIRE }}<br></span>
               </p>
               <div class="social-links mt-3">
-                <a v-if="siteStore.settings.FACEBOOK_URL" :href="siteStore.settings.FACEBOOK_URL" class="facebook" target="_blank" rel="noopener"><i class="bi bi-facebook"></i></a>
-                <a v-if="siteStore.settings.TWITTER_URL" :href="siteStore.settings.TWITTER_URL" class="twitter" target="_blank" rel="noopener"><i class="bi bi-twitter-x"></i></a>
-                <a v-if="siteStore.settings.INSTAGRAM_URL" :href="siteStore.settings.INSTAGRAM_URL" class="instagram" target="_blank" rel="noopener"><i class="bi bi-instagram"></i></a>
-                <a v-if="siteStore.settings.YOUTUBE_URL" :href="siteStore.settings.YOUTUBE_URL" class="youtube" target="_blank" rel="noopener"><i class="bi bi-youtube"></i></a>
+                <a v-if="siteStore.settings.FACEBOOK_URL" :href="siteStore.settings.FACEBOOK_URL" target="_blank" rel="noopener" title="Facebook"><i class="bi bi-facebook"></i></a>
+                <a v-if="siteStore.settings.TWITTER_URL" :href="siteStore.settings.TWITTER_URL" target="_blank" rel="noopener" title="Twitter"><i class="bi bi-twitter-x"></i></a>
+                <a v-if="siteStore.settings.INSTAGRAM_URL" :href="siteStore.settings.INSTAGRAM_URL" target="_blank" rel="noopener" title="Instagram"><i class="bi bi-instagram"></i></a>
+                <a v-if="siteStore.settings.YOUTUBE_URL" :href="siteStore.settings.YOUTUBE_URL" target="_blank" rel="noopener" title="YouTube"><i class="bi bi-youtube"></i></a>
+                <a v-if="siteStore.settings.LINKEDIN_URL" :href="siteStore.settings.LINKEDIN_URL" target="_blank" rel="noopener" title="LinkedIn"><i class="bi bi-linkedin"></i></a>
+                <a v-if="siteStore.settings.WHATSAPP_NUMBER" :href="'https://wa.me/' + siteStore.settings.WHATSAPP_NUMBER.replace(/[^0-9]/g, '')" target="_blank" rel="noopener" title="WhatsApp"><i class="bi bi-whatsapp"></i></a>
               </div>
             </div>
           </div>
